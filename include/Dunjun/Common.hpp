461615a1105386d846ed9498c1252f89d817558a
gingerBill <ginger.bill.22@gmail.com>
2015-02-01 16:04:04 +0000

    Hello World Test

---
+++ include/Dunjun/Common.hpp
@@ -0,0 +1,10 @@
+#ifndef DUNJUN_COMMON_HPP
+#define DUNJUN_COMMON_HPP
+
+namespace Dunjun
+{
+using Float32 = float;
+using Float64 = double;
+} // namespace Dunjun
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Dunjun/Common.hpp>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hellope!\n";
+	std::cin.ignore();
+
+	return 0;
+}
